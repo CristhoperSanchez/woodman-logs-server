ab76bc7afaedbed68834d06a67a2b031e109f65c
CristhoperSanchez <cris.israel.sanchez@gmail.com>
2025-06-20 19:36:00 +0000

    Commiting changess

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express, { application } from 'express';
+import dotenv from 'dotenv';
+import DB from './data/database.js';
+
+
+dotenv.config()
+const {
+  PORT
+} = process.env
+
+
+const APP = express();
+APP.use(express.json());
+
+
+
+APP.post('/logging/addlog', (req, res) =>{
+  console.log("Logging request Detected: ", req.body);
+  DB.InsertLog(req.body);
+  res.status(200).send("That worked :)")
+})
+
+
+APP.get('/loggingstructure', (req, res) =>{
+  res.json({
+    "application" : "text",
+    "environment" : "text",
+    "statusCode"  : "int",
+    "level"  : "text ( fatal, error, warn, info, debug, trace )",
+    "status" : "text",
+    "stack"  : "text",
+    "message" : "text",
+    "date": "date"
+  })
+})
+
+APP.listen(PORT, () => {
+  console.clear();
+  console.log("App Listening on Port: ", PORT)
+})
---
+++ src/data/database.js
@@ -0,0 +1,91 @@
+import DatabaseConstructor from 'better-sqlite3';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Resolve path to ./data/pdf.db in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const dbPath = path.resolve(__dirname, 'pdf.db');
+
+const db = new DatabaseConstructor(dbPath);
+
+class DataBaseManager {
+    constructor(database) {
+        this.DB = database;
+    }
+
+    PrepareDatabase() {
+
+        try {
+            this.DB.prepare(`
+                CREATE TABLE IF NOT EXISTS event_logs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                application TEXT,
+                environment TEXT,
+                statusCode INTEGER,
+                level TEXT CHECK(level IN ('Fatal', 'Error', 'Warn', 'Info', 'Debug', 'Trace')) NOT NULL,
+                status TEXT,
+                dateRecieved DATETIME DEFAULT CURRENT_TIMESTAMP,
+                stack TEXT,
+                message TEXT,
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+            )`).run();
+        } catch(error) {
+            console.error("Error Loading/Creating Database")
+            console.error(error)
+
+        }
+    }
+
+    InsertLog(logObject){
+        /* Check Object type function */
+        try{
+             console.log("values being inserted: ", 
+                logObject?.application ,
+                logObject?.environment ,
+                logObject?.statusCode ,
+                logObject?.level ,
+                logObject?.status ,
+                logObject?.stack ,
+                logObject?.message ,
+                logObject?.timestamp 
+            );
+            const stmt = this.DB.prepare(`
+                INSERT INTO event_logs (
+                    application, environment, statusCode, level,
+                    status, stack, message, timestamp
+                )
+                `)
+
+  
+
+            const result = stmt.run(
+                logObject.application ,
+                logObject.environment ,
+                logObject.statusCode ,
+                logObject.level ,
+                logObject.status ,
+                logObject.stack ,
+                logObject.message ,
+                logObject.timestamp 
+            )
+
+
+            console.log("Result: ", result);
+            return result
+        } catch( error ) {
+            console.log("Error adding record: ", error)
+            return
+        }
+    }
+
+
+
+}
+
+
+// Usage
+export const DB = new DataBaseManager(db);
+DB.PrepareDatabase();
+export default DB; // optional, in case you want default import
